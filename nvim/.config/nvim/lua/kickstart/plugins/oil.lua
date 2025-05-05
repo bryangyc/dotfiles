42124d92c00693285d246af50bf7edbaea06a988
bryangyc <bryan.gohyc@gmail.com>
2025-05-05 12:03:17 +0800

    feat: update oil to include shortcuts

--- nvim/.config/nvim/lua/kickstart/plugins/oil.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/oil.lua
@@ -6,6 +6,16 @@ return {
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  config = function()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+    vim.keymap.set('n', '<space>-', require('oil').toggle_float, { desc = 'Open oil in a floating manner' })
+  end,
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
