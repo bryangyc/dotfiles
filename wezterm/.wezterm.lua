2eccaf0bd370fcbf7b70b6662e82617ba2210788
bryangyc <bryan.gohyc@gmail.com>
2025-05-07 09:51:40 +0800

    feat: add if condition for default domain for wsl usage

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,10 +4,14 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_domain = "WSL:Ubuntu"
+end
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Tokyo Night"
 config.enable_wayland = false
 
 config.window_background_opacity = 0.8
