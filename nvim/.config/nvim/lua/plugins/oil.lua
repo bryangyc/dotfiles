335dd1414bcde4b342f6be8f89e5749eb3e333d2
bryangohyc <bryan.gohyc@gmail.com>
2025-11-09 23:08:00 +0800

    automated commit

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  config = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+    -- vim.keymap.set("n", "<space>-", require("oil").toggle_float, { desc = "Open oil in a floating manner" })
+  end,
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
